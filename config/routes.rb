efc9eeccc71f6d34ac40d8b6366e2a37edc03a8a
Micaela Frank <micaelalfrank@gmail.com>
2022-07-09 13:20:21 -0400

    commit tree 0d6e61e4bfa25d87d7a040d1f1bbee35e39f4331

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create, :show]
+  resources :sessions, only: [:create, :destroy]
+  post "/signup", to: "user#create"
+  get "/me", to: "user#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
